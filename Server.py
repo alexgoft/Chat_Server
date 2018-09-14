54fce2e9b7f19b7d0bd1bd05f5530f685ca1e291
Alex <alex.goft@mail.huji.ac.il>
2018-09-14 20:22:11 +0300

    added support for quiting users

--- Server.py
+++ Server.py
@@ -19,7 +19,6 @@ class Server:
     def __init__(self):
 
         self._server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self._server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self._server_socket.bind(ADDRESS)
         self._server_socket.listen(5)
 
@@ -28,26 +27,34 @@ class Server:
         self._run()
 
     def client_handler(self, client_socket, client_address):
+        try:
+            # TODO IMPLEMENT LOGIC FOR UNIQUE NAMES
 
-        # First message exchange - Get name of client and start communication.
-        client_socket.send(SERVER_WELCOME_MESSAGE.encode())
-        client_name = client_socket.recv(BUFFER_SIZE).decode("utf-8")
+            # First message exchange - Get name of client and start communication.
+            client_socket.send(SERVER_WELCOME_MESSAGE.encode())
+            client_name = client_socket.recv(BUFFER_SIZE).decode("utf-8")
 
-        connected_msg = "{} has connected.".format(client_name)
-        self.broadcast_msg(connected_msg, client_socket=client_socket)
+            connected_msg = "{} has connected.".format(client_name)
+            self.broadcast_msg(connected_msg, client_socket=client_socket)
 
-        self._clients.setdefault(client_socket, {})['client_address'] = client_address
-        self._clients[client_socket]['client_name'] = client_name
+            self._clients.setdefault(client_socket, {})['client_address'] = client_address
+            self._clients[client_socket]['client_name'] = client_name
 
-        while True:
-            client_msg = client_socket.recv(BUFFER_SIZE).decode("utf-8")
-            if client_msg:
+            while True:
+                client_msg = client_socket.recv(BUFFER_SIZE).decode("utf-8")
+                if client_msg:
 
-                if client_msg == Client.CLIENT_QUIT_MESSAGE:
-                    pass
+                    if client_msg == Client.CLIENT_QUIT_MESSAGE:
+                        raise Exception
 
-                client_msg_to_console = "{}: {}".format(self._clients[client_socket]['client_name'], client_msg)
-                self.broadcast_msg(client_msg_to_console)
+                    client_msg_to_console = "{}: {}".format(self._clients[client_socket]['client_name'], client_msg)
+                    self.broadcast_msg(client_msg_to_console)
+        except:
+            self._disconnect_client(client_name, client_socket)
+
+    def _disconnect_client(self, client_name, client_socket):
+        del self._clients[client_socket]
+        self.broadcast_msg('{} disconnected.'.format(client_name))
 
     def broadcast_msg(self, msg, client_socket=None):
         print(msg)
